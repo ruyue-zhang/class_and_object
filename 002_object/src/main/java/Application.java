b83ce99965cb33fe5d9d9cc3d1ea1ff4ca112704
lpdft <772346730@qq.com>
2020-02-08 23:13:44 +0800

    [ruyue] feat: create Teacher object and call Teacher function

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -4,9 +4,16 @@ import model.Teacher;
 public class Application {
 
     public static void main(String[] args) {
-        Student stu = new Student("李莉", 20, "女");
+        Student stu = new Student("李莉", 20, "女" );
         System.out.println(stu.getName() + "," + stu.getAge() + "," + stu.getGender());
         stu.study();
         stu.rest();
+
+        Student[] studentList = new Student[2];
+        studentList[0] = stu;
+        studentList[1] = new Student("王其", 21, "男");
+        Teacher teacher = new Teacher("张龙", studentList);
+        teacher.addStudent(new Student("赵毅", 22, "男"));
+        teacher.showStudentList();
     }
 }
