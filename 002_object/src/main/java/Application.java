97c1ba5763b53964da55c9657bce4cd11adf22f8
lpdft <772346730@qq.com>
2020-02-08 18:18:43 +0800

    [ruyue] feat: create Student class and object

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -1,6 +1,15 @@
+import model.Student;
 public class Application {
 
     public static void main(String[] args) {
-
+        Student stu = new Student();
+        stu.name = "李莉";
+        stu.age = 20;
+        stu.gender = "女";
+        System.out.println(stu.name);
+        System.out.println(stu.age);
+        System.out.println(stu.gender);
+        stu.study();
+        stu.rest();
     }
 }
---
+++ 002_object/src/main/java/model/Student.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Student {
+    public String name;
+    public int age;
+    public String gender;
+    public void study() {
+        System.out.println("Student study!");
+    }
+    public void rest() {
+        System.out.println("Student rest!");
+    }
+}
