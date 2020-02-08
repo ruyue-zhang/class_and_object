327c75965000f0020041494f6ba913156f82cce9
lpdft <772346730@qq.com>
2020-02-08 23:00:39 +0800

    [ruyue] feat: add Student construction function

--- 002_object/src/main/java/Application.java
+++ 002_object/src/main/java/Application.java
@@ -1,11 +1,10 @@
 import model.Student;
+import model.Teacher;
+
 public class Application {
 
     public static void main(String[] args) {
-        Student stu = new Student();
-        stu.setName("李莉");
-        stu.setAge(20);
-        stu.setGender("女");
+        Student stu = new Student("李莉", 20, "女");
         System.out.println(stu.getName() + "," + stu.getAge() + "," + stu.getGender());
         stu.study();
         stu.rest();
--- 002_object/src/main/java/model/Teacher.java
+++ 002_object/src/main/java/model/Teacher.java
@@ -1,4 +1,42 @@
 package model;
 
 public class Teacher {
+    private String name;
+    private Student[] studentList;
+
+    public Teacher() {}
+
+    public Teacher(String name, Student[] studentList) {
+        this.name = name;
+        this.studentList = studentList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student[] getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(Student[] studentList) {
+        this.studentList = studentList;
+    }
+
+    public void addStudent(Student student) {
+        Student[] newStudentList = new Student[this.studentList.length + 1];
+        System.arraycopy(studentList, 0, newStudentList, 0, this.studentList.length);
+        newStudentList[newStudentList.length - 1] = student;
+        this.setStudentList(newStudentList);
+    }
+
+    public void showStudentList() {
+        for (Student student : this.studentList) {
+            System.out.println(student.getName() + "," + student.getAge() + "," + student.getGender());
+        }
+    }
 }
